bd68c63dc95f0abe496cc2629ccbd593ccbed264
davidlamcm <davidlamcm@gmail.com>
2017-04-23 12:13:52 +0800

    add minturnover function

---
+++ R/fun_minTurnover.R
@@ -0,0 +1,16 @@
+#' given a matrix of portfolio composition, minimizee the portfolio turnover
+#' @export
+#' 
+minTurnovr <-function(mat, thershold = 0.05){
+  out = mat
+  pos = mat[1,]
+  for(i in 1:(nrow(out)-1)){
+    if(sqrt(sum((pos-mat[i,])**2))>thershold){
+        out[i, ] = mat[i, ]
+        pos = mat[i, ]      
+    }else{
+        out[i, ] = pos
+      }
+  }
+  out
+}
