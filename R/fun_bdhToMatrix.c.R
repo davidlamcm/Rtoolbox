cd84864484fb68ad64bcb82da5f18f4a531e0d22
davidlamcm <davidlamcm@gmail.com>
2017-06-01 18:53:01 +0800

    small bug fixed

--- R/fun_bdhToMatrix.c.R
+++ R/fun_bdhToMatrix.c.R
@@ -7,60 +7,46 @@
 #'@param lag number of trading day lag, default is 0
 #'@export
 
-
 bdhToMatrix.c <-function(bdhList, timeSeries, validDays =730, lag =0, announcementDate = NULL){
-
+  
   if(!class(timeSeries) =="Date"){
     tryCatch({timeSeries = as.Date(timeSeries)}, error=function(){print("timeSeries is not class Date and failed to convert to Date")})
   }
   timeSeries = sort(timeSeries)
   timeSeries.int = as.numeric(timeSeries)
   timeSeries.char = as.character(timeSeries)
-  
-  #create matrix #'@param announcementDate a list of the same format as bdhList, it can be generated using bdh funciton with field "ANNOUNCEMENT_DT"
-  #'@export
-  
-  
-  bdhToMatrix.c <-function(bdhList, timeSeries, validDays =730, lag =0, announcementDate = NULL){
-    
-    if(!class(timeSeries) =="Date"){
-      tryCatch({timeSeries = as.Date(timeSeries)}, error=function(){print("timeSeries is not class Date and failed to convert to Date")})
-    }
-    timeSeries = sort(timeSeries)
-    timeSeries.int = as.numeric(timeSeries)
-    timeSeries.char = as.character(timeSeries)
-    #replace annoucement Date 
-    if(!is.null(announcementDate)){
-      tryCatch({
-        for(i in names(bdhList)){
-          matchIndex = (match(bdhList[[i]]$date,announcementDate[[i]]$date))
-          if(sum(is.na(matchIndex))>5){
-            warning(paste0("abnormally high number of mismatched announcement date for ", i ))
-          }
-          bdhList[[i]]$date[!is.na(matchIndex)] = announcementDate[[i]]$ANNOUNCEMENT_DT[na.omit(matchIndex)]
+  #replace annoucement Date 
+  if(!is.null(announcementDate)){
+    tryCatch({
+      for(i in names(bdhList)){
+        matchIndex = (match(bdhList[[i]]$date,announcementDate[[i]]$date))
+        if(sum(is.na(matchIndex))>5){
+          warning(paste0("abnormally high number of mismatched announcement date for ", i ))
         }
-      },error= function(e){print("some problem with the annoucement date")})
-    }
-    #create matrix 
-    mappedList = foreach(bdh = bdhList)%do%{
-      bdh.dt =bdh$date
-      bdh.dt.int = as.numeric(bdh$date)
-      vect = bdh[,2]
-      dtPoses = matchDate(bdh.dt.int,timeSeries.int,validDays, lag)
-      dt = as.character(bdh.dt[dtPoses])
-      value = vect[dtPoses]
-      list(dt =dt,value=value)
-    }
-    
-    dateMatrix =do.call(cbind,  foreach(ele = mappedList)%do%{
-      ele$dt
-    })
-    bdhMatrix =do.call(cbind,  foreach(ele = mappedList)%do%{
-      ele$value
-    })
-    colnames(dateMatrix) = names(bdhList)
-    rownames(dateMatrix) = timeSeries.char  
-    colnames(bdhMatrix) = names(bdhList)
-    rownames(bdhMatrix) = timeSeries.char
-    return( list(value  = bdhMatrix, date = dateMatrix))
+        bdhList[[i]]$date[!is.na(matchIndex)] = announcementDate[[i]]$ANNOUNCEMENT_DT[na.omit(matchIndex)]
+      }
+    },error= function(e){print("some problem with the annoucement date")})
   }
+  #create matrix 
+  mappedList = foreach(bdh = bdhList)%do%{
+    bdh.dt =bdh$date
+    bdh.dt.int = as.numeric(bdh$date)
+    vect = bdh[,2]
+    dtPoses = matchDate(bdh.dt.int,timeSeries.int,validDays, lag)
+    dt = as.character(bdh.dt[dtPoses])
+    value = vect[dtPoses]
+    list(dt =dt,value=value)
+  }
+  
+  dateMatrix =do.call(cbind,  foreach(ele = mappedList)%do%{
+    ele$dt
+  })
+  bdhMatrix =do.call(cbind,  foreach(ele = mappedList)%do%{
+    ele$value
+  })
+  colnames(dateMatrix) = names(bdhList)
+  rownames(dateMatrix) = timeSeries.char  
+  colnames(bdhMatrix) = names(bdhList)
+  rownames(bdhMatrix) = timeSeries.char
+  return( list(value  = bdhMatrix, date = dateMatrix))
+}
