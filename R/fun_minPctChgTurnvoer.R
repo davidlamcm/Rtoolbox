110282ce1ed8b3b7592504e670ec36cc4c290531
davidlamcm <davidlamcm@gmail.com>
2017-06-21 21:57:36 +0800

    added a function to minimize turnover only when percentage difference is too large

---
+++ R/fun_minPctChgTurnvoer.R
@@ -0,0 +1,25 @@
+#' given a matrix of portfolio composition, minimize the turnover by only changing the position if target vs current is larger than certain percentage
+#' does not work for negative weight
+#' it does not guarantee wgt add up to 100%
+#' @param mat matrix of turnover
+#' @param thershold percentage of deviation between target and current
+#' @param minvalue min size of change in any case
+#' @export
+#' 
+minPctChgTurnover <-function(mat, thershold = 0.2, minvalue = 0.005){
+  out = mat
+  pos = mat[1,]
+  for(i in 2:(nrow(out))){
+    actionFlag= ((abs(pos/mat[i,]-1))>thershold)
+    out[i, ] = ifelse(actionFlag, mat[i, ], pos)
+    pos = mat[i, ]
+    
+    
+    
+          out[i, ] = mat[i, ]
+      pos = mat[i, ]
+    }else{
+      out[i, ] = pos
+    }
+  out
+}
--- R/fun_minTurnover.R
+++ R/fun_minTurnover.R
@@ -4,10 +4,10 @@
 minTurnover <-function(mat, thershold = 0.05){
   out = mat
   pos = mat[1,]
-  for(i in 1:(nrow(out)-1)){
+  for(i in 2:(nrow(out))){
     if(sum(abs(pos-mat[i,]))>thershold){
         out[i, ] = mat[i, ]
-        pos = mat[i, ]      
+        pos = mat[i, ]
     }else{
         out[i, ] = pos
       }
