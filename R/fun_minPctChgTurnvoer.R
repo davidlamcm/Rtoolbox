8890e222632efbac7d5e0b9ae8ed7ec9bd0ca87e
davidlamcm <davidlamcm@gmail.com>
2017-07-02 12:37:09 +0800

    updated syntax

--- R/fun_minPctChgTurnvoer.R
+++ R/fun_minPctChgTurnvoer.R
@@ -1,14 +1,12 @@
-#' given a matrix of portfolio composition, minimize the turnover by only changing the position if target vs current is larger than certain percentage
-#' does not work for negative weight
-#' it does not guarantee wgt add up to 100%
-#' @param mat matrix of turnover
-#' @param thershold percentage of deviation between target and current
-#' @param minPositionValue min position of an asset class, else will be zero
-#' @param clearanceThershold if theoretical position fall below this thershold, then will clear the existing position
-
-#' @export
-#' 
-minPctChgTurnover <-function(mat, thershold = 0.2, minPositionValue = 0.01, clearanceThershold =0.005){
+#'given a matrix of portfolio composition, minimize the turnover by only changing the position if target vs current is larger than certain percentage
+#'does not work for negative weight
+#'it does not guarantee wgt add up to 100percent
+#'@param mat matrix of turnover
+#'@param thershold percentage of deviation between target and current
+#'@param minPositionValue min position of an asset class, else will be zero
+#'@param clearanceThershold if theoretical position fall below this thershold, then will clear the existing position
+#'@export
+minPctChgTurnover <-function(mat, thershold = 0.2, minPositionValue = 0.01, clearanceThershold = 0.005){
   out = mat
   pos = mat[1,]  #before change position
   for(i in 2:(nrow(out))){
